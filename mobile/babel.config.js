dd356578c8a1a3fec14c481240dbc0054f78a71a
malverneasbuilds <malverneasbuilds@gmail.com>
2026-03-02 00:39:07 +0200

    Fix Expo Web Vite import.meta parsing with Babel AST transformer

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,5 +2,18 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
+        plugins: [
+            function () {
+                return {
+                    visitor: {
+                        MetaProperty(path) {
+                            if (path.node.meta.name === 'import' && path.node.property.name === 'meta') {
+                                path.replaceWithSourceString('({ env: process.env || {} })');
+                            }
+                        }
+                    }
+                };
+            }
+        ]
     };
 };
